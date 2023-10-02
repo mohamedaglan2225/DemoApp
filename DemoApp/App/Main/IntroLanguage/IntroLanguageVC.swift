b6657a03e0c37a41615a38ecef5b12a07cc20fbf
Mohamed Aglan <62311156+mohamedaglan2225@users.noreply.github.com>
2023-10-02 12:08:15 +0300

    Handled Project base

--- DemoApp/App/Main/IntroLanguage/IntroLanguageVC.swift
+++ DemoApp/App/Main/IntroLanguage/IntroLanguageVC.swift
@@ -10,68 +10,27 @@ import UIKit
 class IntroLanguageVC: BaseVC {
     
     //MARK: - IBOutlets -
-    @IBOutlet weak private var arabicButton: UIButton!
-    @IBOutlet weak private var englishButton: UIButton!
+    
     
     //MARK: - Properties -
     
     
-    //MARK: - Creation -
-    static func create() -> IntroLanguageVC {
-        let vc = AppStoryboards.main.instantiate(IntroLanguageVC.self)
-        return vc
-    }
-    
     // MARK: - Lifecycle -
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.configureInitialDesign()
+        
     }
     
     
     //MARK: - Design Methods -
-    private func configureInitialDesign() {
-        self.title = "".localized
-    }
+   
     
     //MARK: - Logic Methods -
-    private func goToNext() {
-        let vc = AppTabBarController.create()
-        AppHelper.changeWindowRoot(vc: vc)
-    }
+   
     
     //MARK: - Actions -
+
     
-    /*
-     if current language is the selected one, No need to change semantic attribute, just go to next page
-     */
-    
-    @IBAction private func arabicButtonPressed() {
-        
-        let vc = LoginVC.create()
-        vc.isModalInPresentation = true
-        let nav = BaseNav(rootViewController: vc)
-        self.present(nav, animated: true)
-        
-//        guard !Language.isRTL() else {
-//            goToNext()
-//            return
-//        }
-//        Language.setAppLanguage(lang: Language.Languages.ar)
-//        UIView.appearance().semanticContentAttribute = .forceRightToLeft
-//        goToNext()
-    }
-    @IBAction private func englishButtonPressed() {
-        
-        guard Language.isRTL() else {
-            goToNext()
-            return
-        }
-        
-        Language.setAppLanguage(lang: Language.Languages.en)
-        UIView.appearance().semanticContentAttribute = .forceLeftToRight
-        goToNext()
-    }
 }
 
 
--- DemoApp/App/Main/Splash/SplashVC.swift
+++ DemoApp/App/Main/Splash/SplashVC.swift
@@ -10,7 +10,6 @@ class SplashVC: BaseVC {
     
     //MARK: - IBOutlets -
     
-    @IBOutlet weak var stackView: UIStackView!
     
     //MARK: - Properties -
     static func create() -> SplashVC {
@@ -21,52 +20,11 @@ class SplashVC: BaseVC {
     // MARK: - UIViewController Events -
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setupDesign()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            self.goNext()
-        }
+        
     }
     
-    //MARK: - Design Methods -
-    func setupDesign() {
-        self.stackView.animateToTop()
-        let window = UIApplication.shared.windows.first
-        window?.backgroundColor = Theme.colors.mainColor
-    }
-    
-    //MARK: - Logic Methods -
-    private func goNext() {
-        switch UserDefaults.isFirstTime {
-        case true:
-            self.goToLanguage()
-        case false:
-            self.goToHome()
-        }
-    }
-    
-    //MARK: - Route Methods -
-    private func goToLanguage() {
-        let vc = IntroLanguageVC.create()
-        AppHelper.changeWindowRoot(vc: vc)
-    }
-    
-    private func goToIntro() {
-        var vcs = [UIViewController]()
-
-        for i in 1 ... 5 {
-            vcs.append(IntroVC.create(image: "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg", title: "title \(i)", body: "body \(i)"))
-        }
-
-
-        let vc = IntroPageController.create(subVCs: vcs)
-        AppHelper.changeWindowRoot(vc: vc)
-    }
-    
-    private func goToHome() {
-        let vc = AppTabBarController.create()
-        AppHelper.changeWindowRoot(vc: vc)
-    }
     
     //MARK: - IBActions -
+    
     
 }
